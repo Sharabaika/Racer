c693beff161ef90b072ef8e96c35f550b2ca7c3e
Sharabaika <sharobaicko@gmail.com>
2023-11-07 00:46:11 +0100

    Init

---
+++ Source/Racer/RacerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACER_API ARacerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Racer/Vehicles/VehicleBase.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "VehicleBase.generated.h"
+
+
+UCLASS(Blueprintable)
+class RACER_API AVehicleBase : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+};
